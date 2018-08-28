02ea7d32fd155a5121c62a2b27704c5c992a9082
Artem Vorotnikov <artem@vorotnikov.me>
2018-08-28 17:46:43 +0300

    Fail on error

--- API_tests/test_api_2.0.py
+++ API_tests/test_api_2.0.py
@@ -27,53 +27,41 @@ def action(dictq):
     }
     context['regmail'] = 'test' + context['n'] + '@test.test'
     for i in dictq:
-        try:
-            answer = request(json.loads(dictq[i] % context), token_headers, cookie)
-            if dictq[i] == q.queries['adm_token']:
-                ad_token = answer.json()['data']['getJWTByEmail']['token']
-                token_headers = {'Authorization': 'Bearer ' + ad_token}
-            elif dictq[i] == q.queries['cr_cat']:
-                context['cat_id'] = answer.json()['data']['createCategory']['id']
-                context['cat_rawid'] = answer.json()['data']['createCategory']['rawId']
-            elif dictq[i] == q.queries['cr_attr']:
-                context['attr_id'] = answer.json()['data']['createAttribute']['id']
-                context['attr_rawid'] = answer.json()['data']['createAttribute']['rawId']
-            elif dictq[i] == q.queries['user_token']:
-                token = answer.json()['data']['getJWTByEmail']['token']
-                token_headers = {'Authorization': 'Bearer '+ token}
-            elif dictq[i] == q.queries['user_id']:
-                context['usr_id'] = answer.json()['data']['me']['id']
-                context['usr_rawId'] = answer.json()['data']['me']['rawId']
-            elif dictq[i] == q.queries['createUserDeliveryAddress']:
-                context['addr_id'] = answer.json()['data']['createUserDeliveryAddress']['id']
-                context['addr_rawid'] = answer.json()['data']['createUserDeliveryAddress']['rawId']
-            elif dictq[i] == q.queries['cr_store']:
-                context['store_id'] = answer.json()['data']['createStore']['id']
-                context['store_rawid'] = answer.json()['data']['createStore']['rawId']
-            elif dictq[i] == q.queries['cr_warehouse']:
-                context['war_id'] = answer.json()['data']['createWarehouse']['id']
-            elif dictq[i] == q.queries['cr_b_prod']:
-                context['b_prod_id'] = answer.json()['data']['createBaseProduct']['id']
-                context['b_prod_rawid'] = answer.json()['data']['createBaseProduct']['rawId']
-            elif dictq[i] == q.queries['cr_prod']:
-                context['prod_id'] = answer.json()['data']['createProduct']['id']
-                context['prod_rawid'] = answer.json()['data']['createProduct']['rawId']
-            elif dictq[i] == q.queries['cr_order']:
-                context['order_slug'] = answer.json()['data']['createOrders']['invoice']['orders'][0]['slug']
-            print(answer.json())
-            if 'errors' in answer.text:
-                error_message = 'ЕСТЬ ОШИБКА В ЗАПРОСЕ: ' + str(i) + answer.text
-                errors['message'+str(count)] = error_message
-                count += 1
-        except Exception as e:
-            errors['except'+str(count)] = 'ИСКЛЮЧЕНИЕ В ЗАПРОСЕ ' + i + '\n' + answer.text + '\n' + str(e)
-            count += 1
-            break
-    if count > 0:
-        for i in errors:
-            print ('\n', '\n', errors[i])
-    else:
-        print ('\n', '\n', 'ОШИБОК НЕ ОБНАРУЖЕНО')
+        answer = request(json.loads(dictq[i] % context), token_headers, cookie)
+        if dictq[i] == q.queries['adm_token']:
+            ad_token = answer.json()['data']['getJWTByEmail']['token']
+            token_headers = {'Authorization': 'Bearer ' + ad_token}
+        elif dictq[i] == q.queries['cr_cat']:
+            context['cat_id'] = answer.json()['data']['createCategory']['id']
+            context['cat_rawid'] = answer.json()['data']['createCategory']['rawId']
+        elif dictq[i] == q.queries['cr_attr']:
+            context['attr_id'] = answer.json()['data']['createAttribute']['id']
+            context['attr_rawid'] = answer.json()['data']['createAttribute']['rawId']
+        elif dictq[i] == q.queries['user_token']:
+            token = answer.json()['data']['getJWTByEmail']['token']
+            token_headers = {'Authorization': 'Bearer '+ token}
+        elif dictq[i] == q.queries['user_id']:
+            context['usr_id'] = answer.json()['data']['me']['id']
+            context['usr_rawId'] = answer.json()['data']['me']['rawId']
+        elif dictq[i] == q.queries['createUserDeliveryAddress']:
+            context['addr_id'] = answer.json()['data']['createUserDeliveryAddress']['id']
+            context['addr_rawid'] = answer.json()['data']['createUserDeliveryAddress']['rawId']
+        elif dictq[i] == q.queries['cr_store']:
+            context['store_id'] = answer.json()['data']['createStore']['id']
+            context['store_rawid'] = answer.json()['data']['createStore']['rawId']
+        elif dictq[i] == q.queries['cr_warehouse']:
+            context['war_id'] = answer.json()['data']['createWarehouse']['id']
+        elif dictq[i] == q.queries['cr_b_prod']:
+            context['b_prod_id'] = answer.json()['data']['createBaseProduct']['id']
+            context['b_prod_rawid'] = answer.json()['data']['createBaseProduct']['rawId']
+        elif dictq[i] == q.queries['cr_prod']:
+            context['prod_id'] = answer.json()['data']['createProduct']['id']
+            context['prod_rawid'] = answer.json()['data']['createProduct']['rawId']
+        elif dictq[i] == q.queries['cr_order']:
+            context['order_slug'] = answer.json()['data']['createOrders']['invoice']['orders'][0]['slug']
+        print(answer.json())
+        if 'errors' in answer.text:
+            raise Exception(answer.text)
 
 
 action(q.queries)
