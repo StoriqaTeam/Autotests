076b4244312494d274af5c56fc227b248411029a
Max S <suchkov.m.u@gmail.com>
2018-10-05 12:56:21 +0300

    fix connect to grid 2

--- Selenium/fronttests.py
+++ Selenium/fronttests.py
@@ -21,7 +21,7 @@ driver = webdriver.Remote(
      command_executor='http://uxtest.stq.cloud:4444/wd/hub',
      desired_capabilities=DesiredCapabilities.CHROME)
 # driver = webdriver.Chrome()
-driver.maximize_window()
+#driver.maximize_window()
 driver.get(url)
 driver.implicitly_wait(4)
 assert "Storiqa" in driver.title
