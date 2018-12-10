4551e22d9505a062779e9c66627c0152bfacc513
busarovalex <busarovalex@yandex.ru>
2018-12-10 12:12:34 +0300

    Add create base product test

---
+++ functional-tests/src/query/create_base_product.rs
@@ -0,0 +1,42 @@
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.json",
+    query_path = "graphql/queries/create_base_product.graphql",
+)]
+pub struct CreateBaseProductMutation;
+
+pub use self::create_base_product_mutation::*;
+
+pub fn default_create_base_product_input() -> CreateBaseProductInput {
+    CreateBaseProductInput {
+        client_mutation_id: "".to_string(),
+        name: vec![TranslationInput {
+            lang: Language::EN,
+            text: "Base product".to_string(),
+        }],
+        store_id: 0,
+        short_description: vec![TranslationInput {
+            lang: Language::EN,
+            text: "Base product short description".to_string(),
+        }],
+        long_description: Some(vec![TranslationInput {
+            lang: Language::EN,
+            text: "Base product long description".to_string(),
+        }]),
+        seo_title: Some(vec![TranslationInput {
+            lang: Language::EN,
+            text: "Base product seo title".to_string(),
+        }]),
+        seo_description: Some(vec![TranslationInput {
+            lang: Language::EN,
+            text: "Base product seo description".to_string(),
+        }]),
+        currency: Currency::STQ,
+        category_id: 0,
+        slug: Some("base-product-slug".to_string()),
+        length_cm: Some(10),
+        width_cm: Some(10),
+        height_cm: Some(10),
+        weight_g: Some(1000),
+    }
+}
