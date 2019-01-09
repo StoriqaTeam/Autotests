6b4c24b63af55b348c26ee0d5df022e87675a243
Daniil Kolesnichenko <d.s.kolesnichenko@yandex.ru>
2019-01-09 15:41:07 +0300

    Wizard store functional tests (#33)
    * Add wizard store tests
    
    * rustfmt
    
    * Cleanup (unused variables)
    
    * Update coupon tests
    
    * Rewrite package tests
    
    * Cleanup
    
    * GetMe.wizardStores -> GetWizardStores.wizardStores
    
    * Refactor package tests
    
    * Fix merge errors
    
    * Add delete_company_package test
    
    * Cargo fmt
    
    * Remove duplicating function defs from basic_tests.rs
    
    * Remove duplicating function defs from basic_tests.rs
    
    * Remove duplicating function defs from basic_tests.rs
    
    * Small fixes

--- functional-tests/src/query/create_wizard_store.rs
+++ functional-tests/src/query/create_wizard_store.rs
@@ -7,35 +7,32 @@ use request::GraphqlRequest;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/schema.json",
-    query_path = "graphql/queries/delete_coupon.graphql",
+    query_path = "graphql/queries/create_wizard_store.graphql",
     response_derives = "Clone, Debug, PartialEq"
 )]
-pub struct DeleteCouponMutation;
+pub struct CreateWizardStoreMutation;
 
-pub use self::delete_coupon_mutation::*;
+pub use self::create_wizard_store_mutation::*;
 
-pub type GraphqlRequestOutput = RustDeleteCouponDeleteCoupon;
+pub struct CreateWizardStoreInput;
+pub type GraphqlRequestOutput = RustCreateWizardStoreCreateWizardStore;
 
-pub struct DeleteCouponInput {
-    pub id: i64,
-}
-
-impl GraphqlRequest for DeleteCouponInput {
+impl GraphqlRequest for CreateWizardStoreInput {
     type Output = GraphqlRequestOutput;
 
     fn response(body: serde_json::Value) -> Result<GraphqlRequestOutput, FailureError> {
         let response_body: Response<ResponseData> = serde_json::from_value(body)?;
         match (response_body.data, response_body.errors) {
-            (Some(data), None) => Ok(data.delete_coupon),
+            (Some(data), None) => Ok(data.create_wizard_store),
             (None, Some(errors)) => Err(::failure::format_err!("{:?}", errors)),
             _ => unreachable!(),
         }
     }
 }
 
-impl From<DeleteCouponInput> for serde_json::Value {
-    fn from(val: DeleteCouponInput) -> serde_json::Value {
-        let request_body = DeleteCouponMutation::build_query(Variables { id: val.id });
-        serde_json::to_value(request_body).expect("failed to serialize DeleteCouponInput")
+impl From<CreateWizardStoreInput> for serde_json::Value {
+    fn from(_val: CreateWizardStoreInput) -> serde_json::Value {
+        let request_body = CreateWizardStoreMutation::build_query(Variables {});
+        serde_json::to_value(request_body).expect("failed to serialize CreateWizardStoreInput")
     }
 }
