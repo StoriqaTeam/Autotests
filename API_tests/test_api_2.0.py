19a8a9a3a24d8a32d48e5f2cf8c97ccba47b3128
Serejka <5125402@mail.ru>
2019-01-22 11:35:08 +0300

    Update test_api_2.0.py

--- API_tests/test_api_2.0.py
+++ API_tests/test_api_2.0.py
@@ -149,7 +149,7 @@ def action(dictq:dict):
                 context['order_slug'] = answer.json()['data']['createOrders']['invoice']['orders'][0]['slug']
             elif dictq[i] == q.queries['buyNow']:
                 context['invoice_id'] = answer.json()['data']['buyNow']['invoice']['id']
-            else:
+            elif dictq[i] == q.queries['deleteCompanyPackage']:
                 token_headers['Authorization'] = ad_token
             print(answer.json())
             if 'errors' in answer.text:
